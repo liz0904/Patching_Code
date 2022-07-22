14631bd9e6409c62c6187da601521d442a1f3d7e
liz0904 <liz09045@naver.com>
2022-07-22 13:15:33 +0900

    힘들다

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
+
+
+
 include "./config.php";
 if($_GET['page'] == "login"){
+    if(preg_match("/ |\/|\(|\)|\||&|select|onload|onerror|alert|curl|from|0x/i",$input['id'])) exit("no hack");
+    if(preg_match("/#|select|\(| |where|or|from|where|limit|=|0x/i",$input['id'])) exit("no hack");
+
     try{
         $input = json_decode(file_get_contents('php://input'), true);
     }
@@ -9,8 +15,18 @@ if($_GET['page'] == "login"){
         exit("<script>alert(`wrong input`);history.go(-1);</script>");
     }
     $db = dbconnect();
-    $query = "select id,pw from member where id='{$input['id']}'";
-    $result = mysqli_fetch_array(mysqli_query($db,$query));
+
+    $query = "select id,pw from member where id=?";
+    $q = $db->prepare( $query );
+    $q->bind_param( 's', $input['id']);
+    $q->execute();
+    $result = mysqli_fetch_array($q->get_result());
+
+    ///////admin 로그인 시 비밀번호 암호화////////////
+    if($input['id']=='admin'){
+       $result['pw']=hash("sha256",$result['pw']);
+    }
+    $input['pw']=hash("sha256",$input['pw']);
     if($result['id'] && $result['pw'] == $input['pw']){
         $_SESSION['id'] = $result['id'];
         exit("<script>alert(`login ok`);location.href=`/`;</script>");
@@ -18,6 +34,11 @@ if($_GET['page'] == "login"){
     else{ exit("<script>alert(`login fail`);history.go(-1);</script>"); }
 }
 if($_GET['page'] == "join"){
+    if(preg_match("/ |\/|\(|\)|\||&|select|onload|onerror|alert|curl|from|0x/i",$input['id'])) exit("no hack");
+    if(preg_match("/#|select|\(| |where|or|from|where|limit|=|0x/i",$input['id'])) exit("no hack");
+    if(preg_match("/ |\/|\(|\)|\||&|select|onload|onerror|alert|curl|from|0x/i",$input['email'])) exit("no hack");
+    if(preg_match("/#|select|\(| |where|or|from|where|limit|=|0x/i",$input['pw'])) exit("no hack");
+
     try{
         $input = json_decode(file_get_contents('php://input'), true);
     }
@@ -27,12 +48,33 @@ if($_GET['page'] == "join"){
     $db = dbconnect();
     if(strlen($input['id']) > 256) exit("<script>alert(`userid too long`);history.go(-1);</script>");
     if(strlen($input['email']) > 120) exit("<script>alert(`email too long`);history.go(-1);</script>");
+    ////////////////안전한 비밀번호 설정/////////////////////////
+    $pw=$input['pw'];
+    $num_check=preg_match('/[0-9]/u', $pw);
+    $eng_check=preg_match('/|[a-z]/u', $pw);
+    $big_eng_check=preg_match('/|[A-Z]/u', $pw);
+    $spe_check=preg_match('/\!|\@|\#|\$|\%|\^|\&|\*|/u', $pw);
+    if($num_check==0 || $eng_check==0 || $big_eng_check==0 || $spe_check==0){
+       exit("<script>alert(`no secure password!`);history.go(-1);</script>");
+    }
     if(!filter_var($input['email'],FILTER_VALIDATE_EMAIL)) exit("<script>alert(`wrong email`);history.go(-1);</script>");
-    $query = "select id from member where id='{$input['id']}'";
-    $result = mysqli_fetch_array(mysqli_query($db,$query));
+    
+    $input['id']=mysqli_real_escape_string($db, $input['id']);
+    $input['email']=mysqli_real_escape_string($db, $input['email']);
+    $input['pw']=mysqli_real_escape_string($db, $input['pw']);
+
+    $query = "select id,pw from member where id=?";
+    $q = $db->prepare( $query );
+    $q->bind_param( 's', $input['id']);
+    $q->execute();
+    $result = mysqli_fetch_array($q->get_result());
     if(!$result['id']){
-        $query = "insert into member values('{$input['id']}','{$input['email']}','{$input['pw']}','user')";
-        mysqli_query($db,$query);
+        ///////////회원가입 시 비밀번호 암호화//////////////
+        $input['pw']=hash("sha256",$input['pw']);
+        $query = "insert into member values(?,?,?,'user')";
+        $q = $db->prepare( $query );
+        $q->bind_param( 'sss', $input['id'], $input['email'], $input['pw']);
+        $q->execute();
         exit("<script>alert(`join ok`);location.href=`/`;</script>");
     }
     else{
@@ -40,11 +82,16 @@ if($_GET['page'] == "join"){
     }
 }
 if($_GET['page'] == "upload"){
+    if(preg_match("/ |\/|\(|\)|\||&|select|onload|onerror|alert|curl|from|0x/i",$_GET['no'])) exit("no hack");
+    if(preg_match("/#|select|\(| |where|or|from|where|limit|=|0x/i",$_GET['no'])) exit("no hack");
+
     if(!$_SESSION['id']){
         exit("<script>alert(`login plz`);history.go(-1);</script>");
     }
     if($_FILES['fileToUpload']['size'] >= 1024 * 1024 * 1){ exit("<script>alert(`file is too big`);history.go(-1);</script>"); } // file size limit(1MB). do not remove it.
     $extension = explode(".",$_FILES['fileToUpload']['name'])[1];
+    $input= explode(".",$_FILES['fileToUpload']['name'])[0];
+
     if($extension == "txt" || $extension == "png"){
         system("cp {$_FILES['fileToUpload']['tmp_name']} ./upload/{$_FILES['fileToUpload']['name']}");
         exit("<script>alert(`upload ok`);location.href=`/`;</script>");
@@ -54,17 +101,35 @@ if($_GET['page'] == "upload"){
     }
 }
 if($_GET['page'] == "download"){
+    if(preg_match("/ |\/|\(|\)|\||&|select|onload|onerror|alert|curl|from|0x/i",$_GET['no'])) exit("no hack");
+    if(preg_match("/#|select|\(| |where|or|from|where|limit|=|0x/i",$_GET['no'])) exit("no hack");
+
     $content = file_get_contents("./upload/{$_GET['file']}");
     if(!$content){
         exit("<script>alert(`not exists file`);history.go(-1);</script>");
     }
-    else{
-        header("Content-Disposition: attachment;");
-        echo $content;
-        exit;
+
+    ///////////////////파일 다운로드 시 확장자 제한////////////////////////////////
+    $filepath="./upload/{$_GET['file']}";
+    $extension = pathinfo($filepath, PATHINFO_EXTENSION);
+    if($extension == "txt" || $extension == "png"){
+        if(!$content){
+            exit("<script>alert(`not exists file`);history.go(-1);</script>");
+        }
+        else{
+            header("Content-Disposition: attachment;");
+            echo $input;
+            exit;
+        }
+    }
+    else{        
+        exit("<script>alert(`txt or png only`);history.go(-1);</script>");
     }
 }
 if($_GET['page'] == "admin"){
+    if(preg_match("/ |\/|\(|\)|\||&|select|onload|onerror|alert|curl|from|0x/i",$_GET['no'])) exit("no hack");
+    if(preg_match("/#|select|\(| |where|or|from|where|limit|=|0x/i",$_GET['no'])) exit("no hack");
+
     $db = dbconnect();
     $result = mysqli_fetch_array(mysqli_query($db,"select id from member where id='{$_SESSION['id']}'"));
     if($result['id'] == "admin"){
